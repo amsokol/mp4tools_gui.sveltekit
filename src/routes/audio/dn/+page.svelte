7b5377ad442feb286fa6541eead66be5b91946b3
Aleksandr Sokolovskii <amsokol+github@gmail.com>
2022-11-06 19:46:07 +0300

    intermediate commit
    Signed-off-by: Aleksandr Sokolovskii <amsokol+github@gmail.com>

--- src/routes/audio/dn/+page.svelte
+++ src/routes/audio/dn/+page.svelte
@@ -1,1 +1,33 @@
-<p>daasdsadsadadsada</p>
+<nav class="flex" aria-label="Breadcrumb">
+	<ol class="inline-flex items-center space-x-1 md:space-x-2 pt-1 pl-1">
+		<li class="inline-flex items-center">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="currentColor"
+				viewBox="0 0 16 16"
+				class="mr-1 w-5 h-5 text-gray-500 dark:text-gray-400"
+				aria-hidden="true"
+			>
+				<path
+					d="M10.304 3.13a1 1 0 0 1 1.196.98v1.8l-2.5.5v5.09c0 .495-.301.883-.662 1.123C7.974 12.866 7.499 13 7 13c-.5 0-.974-.134-1.338-.377-.36-.24-.662-.628-.662-1.123s.301-.883.662-1.123C6.026 10.134 6.501 10 7 10c.356 0 .7.068 1 .196V4.41a1 1 0 0 1 .804-.98l1.5-.3z"
+				/>
+				<path
+					d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z"
+				/>
+			</svg>
+			<span class="text-sm font-medium text-gray-500  dark:text-gray-400">Audio</span>
+		</li>
+		<li aria-current="page">
+			<div class="flex items-center">
+				<svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
+					><path
+						fill-rule="evenodd"
+						d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+						clip-rule="evenodd"
+					/></svg
+				>
+				<span class="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">Dialogue normalization</span>
+			</div>
+		</li>
+	</ol>
+</nav>
