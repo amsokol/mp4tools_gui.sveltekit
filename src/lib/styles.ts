fbccf6a5222d23a31d31367b80b31411b9030559
Aleksandr Sokolovskii <amsokol+github@gmail.com>
2022-11-07 22:38:42 +0300

    added opacity for disabled controls
    Signed-off-by: Aleksandr Sokolovskii <amsokol+github@gmail.com>

--- src/lib/styles.ts
+++ src/lib/styles.ts
@@ -4,7 +4,7 @@ export const classInput =
     + ' dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500'
     + ' dark:focus:border-blue-500'
 export const classInputDisabled =
-    ' disabled:cursor-not-allowed disabled:text-gray-400 dark:disabled:text-gray-400'
+    ' disabled:cursor-not-allowed disabled:opacity-40 disabled:text-gray-400 dark:disabled:text-gray-400'
 export const classInputInvalid =
     ' invalid:bg-red-50 invalid:border invalid:border-red-500 invalid:focus:ring-red-500 invalid:focus:border-red-500'
     + ' invalid:dark:bg-gray-700 invalid:dark:border-red-500 invalid:dark:focus:border-red-500'
@@ -15,7 +15,7 @@ export const classCheckbox =
     ' text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600'
     + ' dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
 export const classCheckboxDisabled =
-    ' disabled:cursor-not-allowed disabled:text-gray-600'
+    ' disabled:cursor-not-allowed disabled:text-gray-600 disabled:opacity-40'
 
 // Navbar
 export const classNavbarMenuTopLevel =
